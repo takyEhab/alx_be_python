1b330e2a7d07c6bf9ead84bcb0c27943ff0dfe22
takyEhab <tkyehab@gmail.com>
2024-12-21 21:05:07 +0200

    last task

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,3 +1,15 @@
 task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
+
+massage =  " that requires immediate attention today!" if time_bound == "yes" else ". Consider completing it when you have free time."
+
+match priority:
+    case "high":
+        print(f"Reminder: '{task}' is a {priority} priority task{massage}.")
+    case "medium":
+        print(f"Reminder: '{task}' is a {priority} priority task{massage}.")        
+    case "low":
+        print(f"Note: '{task}' is a {priority} priority task{massage}.")        
+    case _:
+        print("Invalid priority.")
