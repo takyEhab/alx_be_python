a6a60ceedd5c893998ae3abcfb505ee417453b92
takyEhab <tkyehab@gmail.com>
2025-01-12 17:16:09 +0200

    Arithmetic Operations Function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,19 +1,14 @@
 def perform_operation(num1: float , num2: float, operation: str):
     match operation:
         case "add":
-            answer = num1 + num2
+            return num1 + num2
         case "subtract":
-            answer = num1 - num2
-            print(f"The result is {num1 - num2}.")
+            return num1 - num2
         case "multiply":
-            answer = num1 * num2
-            print(f"The result is {num1 * num2}.")
+            return num1 * num2
         case "divide":
             if num2 == 0:
                 return "Cannot divide by zero."
-            answer = num1 / num2
-            print(f"The result is {num1 / num2}.")
+            return num1 / num2
         case _:
-            answer = "Invalid operation."
-            print("Invalid operation.")
-    return answer
+            return "Invalid operation."
