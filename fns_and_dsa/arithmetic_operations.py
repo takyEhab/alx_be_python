020f3116f3c2ce9c34c221105039f76aa667df36
takyEhab <tkyehab@gmail.com>
2025-01-12 17:45:51 +0200

    fixing Arithmetic Operations Function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,26 @@
 def perform_operation(num1 , num2, operation):
-    match operation:
-        case "add":
-            return num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "multiply":
-            return num1 * num2
-        case "divide":
-            if num2 == 0:
-                return "Cannot divide by zero."
-            return num1 / num2
-        case _:
-            return "Invalid operation."
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "Cannot divide by zero."
+        return num1 / num2
+    
+# def perform_operation(num1, num2, operation):
+#     match operation:
+#         case "add":
+#             return num1 + num2
+#         case "subtract":
+#             return num1 - num2
+#         case "multiply":
+#             return num1 * num2
+#         case "divide":
+#             if num2 == 0:
+#                 return "Cannot divide by zero."
+#             return num1 / num2
+#         case _:
+#             return "Invalid operation."
