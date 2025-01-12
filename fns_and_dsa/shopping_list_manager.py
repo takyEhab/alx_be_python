a4f897ce54aeb4e951b87b8e4c036eb0357f4e1d
takyEhab <tkyehab@gmail.com>
2025-01-12 18:03:36 +0200

    fixing Shopping List Manager

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,7 +13,7 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            item = input("Enter new item: ")
+            item = input("Enter the item to add: ")
             if item:
                 shopping_list.append(item)
         elif choice == '2':
