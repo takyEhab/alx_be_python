8db775f55585dd5fd25cc63af2a1a6635fb2c155
takyEhab <tkyehab@gmail.com>
2025-01-22 13:18:23 +0200

    bank account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,15 @@
+class BankAccount:
+    def __init__(self, account_balance = 0):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        self.account_balance += amount
+    def withdraw(self, amount):
+        if amount > self.account_balance:
+            return False
+        else:
+            self.account_balance -= amount
+            return True
+    def display_balance(self):
+        return f"Your balance is {self.account_balance}$"
+
