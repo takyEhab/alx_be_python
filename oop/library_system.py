660f1ce626465ddc272a219eee8aa851b8554a40
takyEhab <tkyehab@gmail.com>
2025-01-30 07:38:15 +0200

    adding str in book class library system classes

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,6 +2,8 @@ class Book:
     def __init__(self, title: str, author: str):
         self.title = title
         self.author = author
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
 
 class EBook(Book):
     def __init__(self, title: str, author: str, file_size: int):
