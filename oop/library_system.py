e0343a0663a0d3e65e42442c20b732c4294c5bb1
takyEhab <tkyehab@gmail.com>
2025-01-30 07:41:59 +0200

    fixing typos in library system classes

--- oop/library_system.py
+++ oop/library_system.py
@@ -24,7 +24,7 @@ class Library:
             if book.__class__.__name__ == 'Book':
                 print(f"Book: {book.title} by {book.author}")
             elif book.__class__.__name__ == 'EBook':
-                print(f"E-Book: {book.title} by {book.author}, File Size: {book.file_size}KB")
+                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
             elif book.__class__.__name__ == 'PrintBook':
-                print(f"Print Book: {book.title} by {book.author}, Page Count: {book.page_count}")
+                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
             
